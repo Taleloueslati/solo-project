c6aee6578c51164ec7feb96f42b97990293cfaeb
Taleloueslati <taleloueslati28@gmail.com>
2023-01-27 11:55:04 +0100

    done

---
+++ index.js
@@ -0,0 +1,166 @@
+var countries =[{
+    namecountrie:"Tunisie" ,
+    imgSrc: "./images/tunisia.png",
+    description:"Tunisia is a North African country belonging to the Maghreb. It is bordered to the north and east by the Mediterranean Sea. Its western border opens on Algeria (965 km) and its south-eastern border on Libya (459 km). Its name is derived from that of its capital, Tunis, located in the north of the country." ,
+    location:"northernmost country in Africa ",
+    OfficialLanguage:"Arabic" ,
+    Population:"12,356,117 million (2022)" ,
+},{
+    namecountrie: "Algerie",
+    imgSrc: "./images/Algerie.png",
+    description:"Algeria is a North African country with a Mediterranean coastline and a Saharan desert interior. Many empires have left legacies here, such as the ancient Roman ruins in seaside Tipaza. In the capital, Algiers, Ottoman landmarks like circa-1612 Ketchaoua Mosque line the hillside Casbah quarter, with its narrow alleys and stairways. The city's Neo-Byzantine basilica Notre Dame d’Afrique dates to French colonial rule." ,
+    location: "North African",
+    OfficialLanguage:"Arabic" ,
+    Population:"44,903,225 million (2022)" ,
+},{
+    namecountrie:"Libya" ,
+    imgSrc: "./images/Libya.png",
+    description:"Libya, officially the State of Libya, is a country in the Maghreb region in North Africa. It is bordered by the Mediterranean Sea to the north, Egypt to the east, Sudan to the southeast, Chad to the south, Niger to the southwest, Algeria to the west, and Tunisia to the northwest." ,
+    location: "North Africa",
+    OfficialLanguage:"Arabic" ,
+    Population:"6,812,341 million (2022)",
+},{
+    namecountrie:"Mauritanya" ,
+    imgSrc: "./images/mauritania.png",
+    description:"Mauritania, officially the Islamic Republic of Mauritania, is a sovereign country in Northwest Africa. It is bordered by the Atlantic Ocean to the west, Western Sahara to the north and northwest, Algeria to the northeast, Mali to the east and southeast, and Senegal to the southwest." ,
+    location:"North African" ,
+    OfficialLanguage:"Arabic" ,
+    Population:"4,736,139 million (2022) ",
+},{
+    namecountrie:"Morocco" ,
+    imgSrc: "./images/Morocco.png",
+    description:"Morocco, a North African country bordering the Atlantic Ocean and Mediterranean Sea, is distinguished by its Berber, Arabian and European cultural influences. Marrakesh's medina, a mazelike medieval quarter, offers entertainment in its Djemaa el-Fna square and souks (marketplaces) selling ceramics, jewelry and metal lanterns. The capital Rabat's Kasbah of the Udayas is a 12th-century royal fort overlooking the water." ,
+    location:"North Africa" ,
+    OfficialLanguage:"Arabic" ,
+    Population:"37.5 million (2022)",
+},{
+    namecountrie:"France" ,
+    imgSrc: "./images/france.png",
+    description:"France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower. The country is also renowned for its wines and sophisticated cuisine. Lascaux's ancient cave drawings, Lyon's Roman theater and the vast Palace of Versailles attest to its rich history." ,
+    location: "Europe",
+    OfficialLanguage: "French",
+    Population: "64,626,628 million (2022)",
+},{
+    namecountrie: "Italie",
+    imgSrc: "./images/italie.png",
+    description:"Italy, a European country with a long Mediterranean coastline, has left a powerful mark on Western culture and cuisine. Its capital, Rome, is home to the Vatican as well as landmark art and ancient ruins. Other major cities include Florence, with Renaissance masterpieces such as Michelangelo's David and Brunelleschi's Duomo, Venice, the city of canals, and Milan, Italy's fashion capital." ,
+    location:"Europe" ,
+    OfficialLanguage:"Italian" ,
+    Population:"58.9 million (2022)" ,
+},{
+    namecountrie:"Germany" ,
+    imgSrc: "./images/Germany.png",
+    description: "Germany is a Western European country with a landscape of forests, rivers, mountain ranges and North Sea beaches. It has over 2 millennia of history. Berlin, its capital, is home to art and nightlife scenes, the Brandenburg Gate and many sites relating to WWII. Munich is known for its Oktoberfest and beer halls, including the 16th-century Hofbräuhaus. Frankfurt, with its skyscrapers, houses the European Central Bank.",
+    location:"Europe" ,
+    OfficialLanguage:"German" ,
+    Population:"84.3 million (2022)" ,
+},{
+    namecountrie:"Netherlands" ,
+    imgSrc: "./images/Netherlands.png",
+    description:"The Netherlands, a country in northwestern Europe, is known for a flat landscape of canals, tulip fields, windmills and cycling routes. Amsterdam, the capital, is home to the Rijksmuseum, Van Gogh Museum and the house where Jewish diarist Anne Frank hid during WWII. Canalside mansions and a trove of works from artists including Rembrandt and Vermeer remain from the city's 17th-century Golden Age." ,
+    location:"Europe" ,
+    OfficialLanguage:"Dutch" ,
+    Population: "17.6 million (2022)",
+},{
+    namecountrie:"Suisse" ,
+    imgSrc: "./images/Suisse.png",
+    description: "Switzerland is a mountainous Central European country, home to numerous lakes, villages and the high peaks of the Alps. Its cities contain medieval quarters, with landmarks like capital Bern's Zytglogge clock tower and Lucerne's wooden chapel bridge. The country is also known for its ski resorts and hiking trails. Banking and finance are key industries, and Swiss watches and chocolate are world renowned.",
+    location:"Europe" ,
+    OfficialLanguage: "French, German, Italian, Romansh",
+    Population:  "8,740,472 million (2022)",
+},{
+    namecountrie:"Brazil",
+    imgSrc:"./images/Brazil.png ",
+    description: "Brazil, officially the Federative Republic of Brazil, is the largest country in South America and in Latin America. At 8.5 million square kilometers and with over 217 million people, Brazil is the world's fifth-largest country by area and the seventh most populous." ,
+    location:"South America and in Latin America",  
+    OfficialLanguage:"Portuguese", 
+    Population:"215,313,498 million (2022)",   
+},{
+    namecountrie:"Argentina" ,
+    imgSrc:"./images/argentina.jpg " ,     
+    description:"Argentina, officially the Argentine Republic, is a country in the southern half of South America. Argentina covers an area of 2,780,400 km², making it the second-largest country in South America after Brazil, the fourth-largest country in the Americas, and the eighth-largest country in the world.",         
+    location:"South America",
+    OfficialLanguage:"Spanish",
+    Population:"  45,510,318 million (2022)" ,
+},{
+    namecountrie:"Australia",
+    imgSrc:"./images/australia.png" ,
+    description:"Australia, officially the Commonwealth of Australia, is a sovereign country comprising the mainland of the Australian continent, the island of Tasmania, and numerous smaller islands. With an area of 7,617,930 square kilometres, Australia is the largest country by area in Oceania and the world's sixth-largest country.",
+    location:"Oceania",
+    OfficialLanguage:"English",
+    Population:"25,978,935 million (2022)",
+},{
+    namecountrie:"Japan" ,
+    imgSrc:"./images/japan.jpg" ,
+    description:"Japan is an island country in East Asia. It is situated in the northwest Pacific Ocean and is bordered on the west by the Sea of Japan, extending from the Sea of Okhotsk in the north toward the East China Sea, Philippine Sea, and Taiwan in the south." ,
+    location:"East Asia" ,
+    OfficialLanguage:"Japanese" ,
+    Population:"125.7 million (2022)"
+}]
+
+
+
+var each = function (coll,func){
+    if (Array.isArray(coll)){
+        for (var i =0;i<coll.length;i++){
+        func(coll[i],i)
+    }
+    }
+    else {
+        for (var key in coll){
+            func(coll[key],key)
+        }
+    }
+}
+
+var filter = function(array,predicate){
+    var acc = [];
+    each(array,function(e,i){
+        if (predicate(e,i)){
+            acc.push(e)
+        }
+    })
+    return acc;
+    }
+
+
+$(".searchBtn").on("click",function(e){
+    var filt=filter(countries,function(e,i){
+        var term=$(".searchOption").val()
+       return e.namecountrie.toLowerCase()===term.toLowerCase() 
+   
+
+       
+    })
+
+
+
+    $("#container").empty()
+    $("#container-video").empty()
+    
+    $(".searchOption").val("")
+    each(filt,function(e,i){
+        var div=$('<div " class="flag"><h2 id="cont">location : '+e.location+'</h2></div>')
+        var p=$("<p class='text'>Description: "+e.description+"</p>")
+        var p2=$("<h2>Population: "+e.Population+"</h2>")
+        var imgSrc2=$('<div  class="grid-item"><img class="img1" src="'+e.imgSrc+'"/></div>')
+        var h1=$("<h1 class='namecountrie'>"+e.namecountrie+"</h1><br>")
+        var h2=$("<h2>Official Language: "+e.OfficialLanguage+"</h2>")
+        $("#container").append(imgSrc2).append(h1).append(div).append(h2).append(p2).append(p)
+        })
+        
+        })
+       
+
+        $(".btn").on("click",function home (e){
+            location.reload()
+            var video =$('<video id="bg-video" width="101%" autoplay muted loop>'+
+            '<source src="assets/Earth.mp4" type="video/mp4"></video>') 
+            $("#container-video").replaceWith(video)
+        
+        }
+        )
+
+
+
+        
